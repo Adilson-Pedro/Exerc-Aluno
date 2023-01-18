eec0c04d7197f6b4bfe9c6edae513ebb78e5b234
Adilson Pedro <pedroadilson93@gmail.com>
2023-01-18 14:27:26 -0300

    Adicionar arquivos de projeto.

---
+++ Exerc-Aluno/Aluno.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc_Aluno
+{
+    internal class Aluno
+    {
+
+        // Atributos
+
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+
+        public double CalcularNota()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado()
+        {
+            if (CalcularNota() >= 60.0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - CalcularNota();
+            }
+
+
+
+
+        }
+    }
+}   
---
+++ Exerc-Aluno/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc_Aluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno A = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            A.Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            Console.Write("Nota 1: ");
+            A.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.Write("Nota 2: ");
+            A.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.Write("Nota 3: ");
+            A.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("NOTA FINAL = " + A.CalcularNota().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (A.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + A.NotaRestante().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
